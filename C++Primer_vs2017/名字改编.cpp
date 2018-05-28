f8677f714f83bfa92c0ec11d4489bb5283b03cb5
userName <userEmail>
2018-05-28 15:10:09 +0800

    extern "C"

---
+++ C++Primer_vs2017/名字改编.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+//extern "C" 表示不进行名字改编,按C语言方式进行名字，即C语言不能函数重载
+extern "C" void fun(int a);
+
+int main(void)
+{
+	fun(1);
+
+	system("pause");
+	return 0;
+}
