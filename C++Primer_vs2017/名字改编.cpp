4d6a9b14255a5bbc59262d7d56880c9d187b6867
lirui <821464520@qq.com>
2018-05-28 15:13:35 +0800

    测试userName

--- C++Primer_vs2017/名字改编.cpp
+++ C++Primer_vs2017/名字改编.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-//extern "C" 表示不进行名字改编,按C语言方式进行名字，即C语言不能函数重载
+//extern "C" 表示不进行名字改编,按C语言方式进行名字,即C语言不能函数重载
 extern "C" void fun(int a);
 
 int main(void)
