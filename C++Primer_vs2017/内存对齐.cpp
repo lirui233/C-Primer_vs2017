acc08d1b6565efca107510044b16d1885d2a69a3
userName <userEmail>
2018-05-28 14:08:59 +0800

    添加内存对齐代码

---
+++ C++Primer_vs2017/内存对齐.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	struct Test
+	{
+		char ch;
+		double a;
+	};
+
+	Test test;
+	//cout << &(test.ch) << endl; //输出乱码,C++标准库中I/O类对输出操作符<<重载，在遇到字符型指针时会将其当做字符串名来处理，输出指针所指的字符串。 
+	cout << &(test.a) << endl;
+	cout << sizeof(test) << endl;
+	cout << static_cast<void *>(&test.ch) << endl;
+
+	system("pause");
+	return 0;
+}
