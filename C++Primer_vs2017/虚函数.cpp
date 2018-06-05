349175a3777a1b9a80b1e18e36f35641572b7e40
李锐 <lirui233ww@outlook.com>
2018-06-05 22:27:17 +0800

    2018.6.5.22点27分.home

--- C++Primer_vs2017/虚函数.cpp
+++ C++Primer_vs2017/虚函数.cpp
@@ -19,6 +19,7 @@ public:
 	{
 		cout << "Base Fun2()" << endl;
 	}
+	int m_nData1;
 };
 
 class Derived :public Base
@@ -33,6 +34,7 @@ public:
 	{
 		cout << "Derived Fun1()" << endl;
 	}
+	int m_nData2;
 };
 
 // 若Fun1不是虚函数，则根据p的类型调用相应类中的成员函数
@@ -46,6 +48,14 @@ void T()
 	p->Fun1();
 	p->Fun2();
 	delete p;
+	cout << sizeof(Base) << endl; //8 bytes
+	cout << sizeof(Derived) << endl;//12 bytes
+
+	Base b;
+	size_t **p = reinterpret_cast<size_t **>(&b);
+	typedef void(*FUNC)(void);
+	//FUNC fun = (FUNC)(p[0][0]);
+	//fun();
 }
 
 int main(void)
