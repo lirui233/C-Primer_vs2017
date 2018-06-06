4aedf1aab8003de19c301659699d05bb0a6db0b9
lirui <821464520@qq.com>
2018-06-06 17:02:30 +0800

    2018.6.6.17点02分.office

---
+++ C++Primer_vs2017/ObjectSlicing对象切割.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+
+class CObject
+{
+public:
+	virtual void Serialize()
+	{
+		cout << "Object::Serialize" << endl;
+	}
+};
+
+class CDocument :public CObject
+{
+public:
+	virtual void Serialize()
+	{
+		cout << "CDocument::Serialize" << endl;
+	}
+	void Func()
+	{
+		cout << "CDocument::Func" << endl;
+		Serialize();
+	}
+	int m_nData1;
+};
+
+class CMyDoc :public CDocument
+{
+public:
+	virtual void Serialize()
+	{
+		cout << "CMyDoc::Serialize" << endl;
+	}
+	int m_nData2;
+};
+
+void T()
+{
+	CMyDoc myDoc;
+	CMyDoc *pMyDoc = new CMyDoc;
+
+	myDoc.Func();
+	cout << endl;
+	((CDocument *)(&myDoc))->Func();
+	cout << endl;
+	pMyDoc->Func();
+	cout << endl;
+	((CDocument)myDoc).Func();//myDoc强制转换为CDocument对象，进行了一次向上转型，派生类的特有成员会消失，虚表也会发生变化。完完全全将派生类对象转换为了基类对象，调用了基类的拷贝构造函数，将派生类对象拷贝构造为基类对象
+	cout << endl;
+}
+
+int main(void)
+{
+	T();
+
+	system("pause");
+	return 0;	
+}
--- C++Primer_vs2017/虚函数.cpp
+++ C++Primer_vs2017/虚函数.cpp
@@ -7,15 +7,16 @@ public:
 	Base() { cout << "Base()" << endl; }
 
 	// 如果一个类要作为多态基类，要将析构函数定义成虚函数
-	virtual ~Base()
-	{
-		cout << "~Base()" << endl;
-	}
+	// 验证虚表时，虚析构函数这里报错
+	//virtual ~Base()
+	//{
+	//	cout << "~Base()" << endl;
+	//}
 	virtual void Fun1()
 	{
 		cout << "Base Fun1()" << endl;
 	}
-	void Fun2()
+	virtual void Fun2()
 	{
 		cout << "Base Fun2()" << endl;
 	}
@@ -37,6 +38,7 @@ public:
 	int m_nData2;
 };
 
+typedef void (*FUNC)(void);
 // 若Fun1不是虚函数，则根据p的类型调用相应类中的成员函数
 // 若Fun1是虚函数，则p指向什么类型就调用相应类型的成员函数
 // 动态绑定的虚函数，必须通过基类指针或引用调用
@@ -44,18 +46,23 @@ public:
 // delete必须先调用派生类的析构函数，否则会造成内存泄漏
 void T()
 {
-	Base *p = new Derived;
-	p->Fun1();
-	p->Fun2();
-	delete p;
+	//Base *p = new Derived;
+	//p->Fun1();
+	//p->Fun2();
+	//delete p;
 	cout << sizeof(Base) << endl; //8 bytes
 	cout << sizeof(Derived) << endl;//12 bytes
 
 	Base b;
-	size_t **p = reinterpret_cast<size_t **>(&b);
-	typedef void(*FUNC)(void);
-	//FUNC fun = (FUNC)(p[0][0]);
-	//fun();
+	long **ptr = reinterpret_cast<long **>(&b);
+
+	FUNC fun = (FUNC)ptr[0][1];
+//	fun();
+
+	Derived d;
+	long **ptr1 = reinterpret_cast<long **>(&d);
+	FUNC fun1 = (FUNC)ptr1[0][1];
+	fun1();
 }
 
 int main(void)
